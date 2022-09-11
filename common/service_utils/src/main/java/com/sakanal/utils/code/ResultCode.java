a96922a98f8945bd20644d92b46fbc7e3c9c8bbb
sakanal <sakanal9527@gmail.com>
2022-09-11 19:34:34 +0800

    修改状态码

--- common/service_utils/src/main/java/com/sakanal/utils/code/ResultCode.java
+++ common/service_utils/src/main/java/com/sakanal/utils/code/ResultCode.java
@@ -2,12 +2,12 @@ package com.sakanal.utils.code;
 
 
 public enum ResultCode {
-    SUCCESS(200),
-    NO_RESULT_DATA(201),
-    NOT_FOUND(404),
-    MISSING_PARAMETERS(401),
-    NO_PARAMETERS(402),
-    ERROR(444);
+    SUCCESS(20000),
+    NO_RESULT_DATA(20001),
+    NOT_FOUND(40004),
+    MISSING_PARAMETERS(40001),
+    NO_PARAMETERS(40002),
+    ERROR(44444);
 
     private final Integer key;
 
