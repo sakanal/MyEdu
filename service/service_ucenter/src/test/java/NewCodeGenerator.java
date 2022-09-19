eb21be61d2b08544900319528c455ba530af98b0
sakanal <sakanal9527@gmail.com>
2022-09-20 00:11:58 +0800

    基本完成登录注册接口，通过短信发送随机生成的验证码，并将验证码以电话为key保存在redis中，注册时获取redis中的验证码进行判断

---
+++ service/service_ucenter/src/test/java/NewCodeGenerator.java
@@ -0,0 +1,81 @@
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NewCodeGenerator {
+    public static void main(String[] args) {
+
+        String host = "localhost";
+        String port = "3306";
+        //数据库名
+        String database = "guli";
+        String username = "root";
+        String password = "123456";
+        //作者
+        String author = "sakanal";
+        //服务目录
+        String serviceDir = "D:\\Program Files (x86)\\Developer_tools\\IDEAworkspace\\guli_edu\\guli_edu_api\\service\\service_ucenter";
+        //包名
+        String parent = "com.sakanal";
+        //父模块名
+        String moduleName = "ucenter";
+        //要去除的表前缀
+        String tablePrefix = "";
+
+        List<String> tables = new ArrayList<>();
+        //数据库表名
+        tables.add("ucenter_member");
+
+        FastAutoGenerator.create("jdbc:mysql://" + host + ":" + port + "/" + database, username, password)
+                .globalConfig(builder -> {
+                    builder.author(author)//
+                            .enableSwagger()
+                            .outputDir(serviceDir + "\\src\\main\\java");
+                })
+                .packageConfig(builder -> {
+                    builder.parent(parent)
+                            .moduleName(moduleName)
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, serviceDir + "\\src\\main\\resources\\mapper"))
+                            .entity("entity")
+                            .service("service")
+                            .serviceImpl("service.impl")
+                            .controller("controller")
+                            .mapper("mapper")
+                            .xml("mapper");
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude(tables)
+                            .addTablePrefix(tablePrefix)
+
+                            .serviceBuilder()
+                            .formatServiceFileName("%sService")
+                            .formatServiceImplFileName("%sServiceImpl")
+
+                            .entityBuilder()
+                            .enableLombok()
+                            .logicDeleteColumnName("deleted")
+                            .enableTableFieldAnnotation()
+
+                            .controllerBuilder()
+                            // 映射路径使用连字符格式，而不是驼峰
+                            .enableHyphenStyle()
+                            .formatFileName("%sController")
+                            .enableRestStyle()
+
+                            .mapperBuilder()
+                            //生成通用的resultMap
+                            .enableBaseResultMap()
+                            .superClass(BaseMapper.class)
+                            .formatMapperFileName("%sMapper")
+                            .enableMapperAnnotation()
+                            .formatXmlFileName("%sMapper");
+                })
+                .templateEngine(new VelocityTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+    }
+}
