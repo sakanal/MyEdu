7c2b716f4b568305a83120de3fe5d78cbef88294
sakanal <sakanal9527@gmail.com>
2022-09-20 00:14:03 +0800

    基本完成登录注册接口，通过短信发送随机生成的验证码，并将验证码以电话为key保存在redis中，注册时获取redis中的验证码进行判断

---
+++ service/service_ucenter/src/main/java/com/sakanal/ucenter/handler/MyMetaObjectHandler.java
@@ -0,0 +1,24 @@
+package com.sakanal.ucenter.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject,"gmtCreate", LocalDateTime.class,LocalDateTime.now());
+
+        this.strictUpdateFill(metaObject,"gmtModified", LocalDateTime.class,LocalDateTime.now());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject,"gmtModified", LocalDateTime.class,LocalDateTime.now());
+    }
+}
