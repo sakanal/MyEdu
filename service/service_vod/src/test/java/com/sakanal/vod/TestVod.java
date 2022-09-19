9142f9cd6b0b903ba84b8b76ff665d5be14e310a
sakanal <sakanal9527@gmail.com>
2022-09-19 18:38:36 +0800

    init

--- service/service_vod/src/test/java/com/sakanal/vod/TestVod.java
+++ service/service_vod/src/test/java/com/sakanal/vod/TestVod.java
@@ -12,6 +12,10 @@ import java.util.List;
 public class TestVod {
 
     public static void main(String[] args) throws ClientException {
+        testUploadVideo("",
+                "",
+                "测试代码方式上传",
+                "D:/guli_edu_api – pom.xml (guli_edu_api).mp4");
     }
     private static void testUploadVideo(String accessKeyId, String accessKeySecret, String title, String fileName) {
         UploadVideoRequest request = new UploadVideoRequest(accessKeyId, accessKeySecret, title, fileName);
